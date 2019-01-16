66f662912176577498bd8d85735df69066d69479
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-15 21:59:46 -0800

    created lizard class

---
+++ Zoo/Zoo/Classes/Lizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    //allows for inheritence  child = Lizard : parent = Reptiles
+
+    abstract class Lizard : Reptiles 
+    {
+        public override int Legs { get; set; } = 2;
+
+    }
+}
