eb258f6bba24321ac80e9f536fd74338d54f57b1
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-15 21:43:21 -0800

    created elk class

---
+++ Zoo/Zoo/Classes/Elk.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    //allows for inheritence  child = Elk : parent = Deer
+    class Elk : Deer
+    {
+        public override string Horns { get; set; } = "has horns";
+
+    }
+}
