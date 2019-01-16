b5aa34e8d1f9963126be23f8d3904ce15619659e
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-15 19:38:43 -0800

    created Animal class

---
+++ Zoo/Zoo/Classes/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    class Animals
+    {
+        abstract class Animal
+        {
+            public abstract int Legs { get; set; }
+            public abstract int Teeth { get; set; }
+            public bool Hair { get; set; }
+            public bool Horns { get; set; }
+
+            public abstract void Eat();
+            public abstract void Sleep();
+        }
+
+    }
+}
