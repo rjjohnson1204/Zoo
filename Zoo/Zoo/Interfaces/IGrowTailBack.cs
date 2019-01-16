efaf32c475f8be3a3b65830987f5797898e6d6af
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-15 22:57:34 -0800

    tail interface

---
+++ Zoo/Zoo/Interfaces/IGrowTailBack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    public interface IGrowTailBack
+    {
+        string GrowTailBack();
+    }
+}
--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Zoo.Classes;
+
 
 namespace Zoo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to to the Zoo!!");
+            Gecko gecko = new Gecko();
+            Console.WriteLine("                        ");
+            Console.WriteLine("        The Gecko ");
+            Console.WriteLine("                        ");
+            Console.WriteLine($" I have {gecko.Legs} legs.");
+
+
+
+
+
+
+
+
         }
     }
 }
