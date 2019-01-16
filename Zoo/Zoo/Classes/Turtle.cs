f19f523799f04f78eae0820eb68989f63f43da77
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-15 21:49:29 -0800

    created turtle

---
+++ Zoo/Zoo/Classes/Turtle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    abstract class Turtle : Reptiles
+    {
+        public override int Teeth { get; set; } = 0;
+        public override string Horns { get; set; } = "none";
+
+
+    }
+}
