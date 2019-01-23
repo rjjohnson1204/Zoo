c9378cba37c7a5c96521c71ffd4870293c3979c7
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-23 01:29:55 -0800

    zoo result

--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -8,12 +8,14 @@ namespace Zoo
     public class Program
     {
         static void Main(string[] args)
-        {   
+        {
             //new gecko
+            Console.WriteLine("                        ");
+            Console.WriteLine("                        ");
             Console.WriteLine("Welcome to to the Zoo!!");
             Gecko gecko = new Gecko();
             Console.WriteLine("                        ");
-            Console.WriteLine("        The Gecko ");
+            Console.WriteLine("      ** The Gecko ** ");
             Console.WriteLine("                        ");
             Console.WriteLine($" I have {gecko.Legs} legs.");
             Console.WriteLine(gecko.GrowTailBack());
@@ -23,18 +25,17 @@ namespace Zoo
             //new golden monkey
             Golden_Monkey monkey = new Golden_Monkey();
             Console.WriteLine("                        ");
-            Console.WriteLine("      Golden Monkey  ");
+            Console.WriteLine("    ** Golden Monkey ** ");
             Console.WriteLine("                        ");
             Console.WriteLine("                        ");
             Console.WriteLine($" I have {monkey.Legs} legs.");
-            Console.WriteLine("                        ");
             Console.WriteLine(monkey.Swing());
             Console.WriteLine("                        ");
 
             // new Reindeer
             Reindeer reindeer = new Reindeer();
             Console.WriteLine("                        ");
-            Console.WriteLine("         Reindeer    ");
+            Console.WriteLine("      ** Reindeer **   ");
             Console.WriteLine("                        ");
             Console.WriteLine($" I have {reindeer.Legs} legs.");
             Console.WriteLine("                        ");
@@ -42,18 +43,21 @@ namespace Zoo
             //new Deer
             Elk elk = new Elk();
             Console.WriteLine("                        ");
-            Console.WriteLine("          Elk    ");
+            Console.WriteLine("      ** Elk **    ");
             Console.WriteLine("                        ");
             Console.WriteLine($" I have {elk.Legs} legs.");
             Console.WriteLine("                        ");
 
+
             // new sea turtle
             Sea_turtle turtle = new Sea_turtle();
             Console.WriteLine("                        ");
-            Console.WriteLine("          Sea Turtle    ");
+            Console.WriteLine("       ** Sea Turtle **    ");
             Console.WriteLine("                        ");
             Console.WriteLine($" I have {turtle.Legs} legs.");
+            Console.WriteLine($" I have this many teeth {turtle.Teeth} :D ");
             Console.WriteLine("                        ");
+
 
 
 
